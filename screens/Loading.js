7a30136f96e9e0ccd3ebeb1c07834164565da73f
Darius <dariusz.sobkowicz@gmail.com>
2020-06-05 11:44:17 +0200

    added loading view styles

--- screens/Loading.js
+++ screens/Loading.js
@@ -1,36 +1,73 @@
 import React, {useState, useEffect} from 'react';
 import {StyleSheet} from 'react-native';
-import {Container, Text, Button} from 'native-base';
+import {Container, Text, Button, View} from 'native-base';
+import ProgressCircle from 'react-native-progress-circle';
+
+import Checkmark from '../assets/checkmark.svg';
 
 const Loading = ({navigation}) => {
     const [ready, setReady] = useState(false);
+    const [progress, setProgress] = useState(0);
 
     useEffect(() => {
-        setTimeout(() => {
-            setReady(true);
-        }, 5000);
+        try {
+            // add create account request
+            setTimeout(() => {
+                setReady(true);
+            }, 4000);
+        } catch {}
     }, []);
 
-    if (ready) {
+    useEffect(() => {
+        if (ready) {
+            setProgress(100);
+            setTimeout(() => {
+                navigation.replace('Account');
+            }, 2000);
+            return;
+        }
+
+        if (progress >= 90) {
+            return;
+        }
+
         setTimeout(() => {
-            navigation.replace('Account');
-        }, 2000);
-    }
+            const newProgress = progress + 15;
+            setProgress(newProgress);
+        }, 1000);
+    }, [progress, ready]);
 
     return (
         <Container style={styles.container}>
-            {!ready && <Text>preparing account...</Text>}
-            {ready && <Text>ready</Text>}
+            <View style={styles.item}>
+                <ProgressCircle
+                    percent={progress}
+                    radius={100}
+                    borderWidth={3}
+                    color="#4b4b4b"
+                    shadowColor="#ff8f00"
+                    bgColor="#fcd104">
+                    <Text style={{fontSize: 16}}>
+                        {progress === 100 ? (
+                            <Checkmark />
+                        ) : (
+                            'preparing account...'
+                        )}
+                    </Text>
+                </ProgressCircle>
+            </View>
         </Container>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
-        paddingHorizontal: 50,
-        paddingVertical: 250,
-        display: 'flex',
+        backgroundColor: '#fcd104',
         alignItems: 'center',
+        justifyContent: 'center',
+    },
+    item: {
+        marginBottom: 100,
     },
 });
 
