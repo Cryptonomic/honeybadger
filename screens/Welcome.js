e6894ec11e95d60860aaa46be41954746282612a
Darius <dariusz.sobkowicz@gmail.com>
2020-06-05 09:59:04 +0200

    added welcome view styles

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -1,38 +1,46 @@
 import React from 'react';
-import {StyleSheet, Image} from 'react-native';
-import {Container, Text, Button, Header, View, Grid} from 'native-base';
+import {StyleSheet} from 'react-native';
+import {Container, Text, Button, View} from 'native-base';
 
-import Logo from '../assets/galleon_logo.svg';
+import Logo from '../assets/galleon-logo.svg';
 
 const Welcome = ({navigation}) => {
     const getStarted = () => navigation.replace('Loading');
     return (
-        <Container style={styles.container}>
+        <Container>
             <View style={styles.top}>
                 <Logo />
             </View>
-            <Text>Logo</Text>
-            <Text>Product of Cryptonomic</Text>
-            <Button onPress={getStarted}>
-                <Text>Get Started</Text>
-            </Button>
+            <View style={styles.item}>
+                <Text>Product of Cryptonomic</Text>
+            </View>
+            <View style={styles.item}>
+                <Button style={styles.btn} onPress={getStarted}>
+                    <Text>Get Started</Text>
+                </Button>
+            </View>
         </Container>
     );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: 'red'
-    },
     top: {
         height: '70%',
         backgroundColor: '#fcd104',
         justifyContent: 'center',
+        alignItems: 'center',
     },
-    logo: {
-        width: 100,
-        height: 100
-    }
+    item: {
+        marginTop: 25,
+        alignItems: 'center',
+    },
+    btn: {
+        width: 256,
+        height: 50,
+        justifyContent: 'center',
+        borderRadius: 25,
+        backgroundColor: '#4b4b4b',
+    },
 });
 
 export default Welcome;
