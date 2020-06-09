53e5816d2cac481b5be5d096d4ef92b5617f04ff
Darius <dariusz.sobkowicz@gmail.com>
2020-06-09 10:18:27 +0200

    welcome bg

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -3,16 +3,26 @@ import {StyleSheet} from 'react-native';
 import {Container, Text, Button, View} from 'native-base';
 
 import Logo from '../assets/galleon-logo.svg';
+import Cryptonomic from '../assets/cryptonomic-icon.svg';
+import Wave from '../assets/splash-wave.svg';
+import WaveShadow from '../assets/splash-wave-shadow.svg';
 
 const Welcome = ({navigation}) => {
     const getStarted = () => navigation.replace('Loading');
     return (
         <Container>
+            <Wave style={styles.wave} />
+            <WaveShadow style={styles.waveShadow} />
+            <View style={styles.bg} />
             <View style={styles.top}>
-                <Logo />
+                <Logo style={styles.logo} />
             </View>
             <View style={styles.item}>
-                <Text>Product of Cryptonomic</Text>
+                <View style={styles.text}>
+                    <Text>Product of</Text>
+                    <Cryptonomic style={styles.logoCrytponomic} />
+                    <Text>Cryptonomic</Text>
+                </View>
             </View>
             <View style={styles.item}>
                 <Button style={styles.btn} onPress={getStarted}>
@@ -26,7 +36,6 @@ const Welcome = ({navigation}) => {
 const styles = StyleSheet.create({
     top: {
         height: '70%',
-        backgroundColor: '#fcd104',
         justifyContent: 'center',
         alignItems: 'center',
     },
@@ -40,6 +49,39 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         borderRadius: 25,
         backgroundColor: '#4b4b4b',
+    },
+    text: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    logoCrytponomic: {
+        marginHorizontal: 5,
+    },
+    logo: {
+        marginBottom: 100,
+    },
+    wave: {
+        position: 'absolute',
+        width: '100%',
+        height: '70%',
+    },
+    waveShadow: {
+        position: 'absolute',
+        width: '100%',
+        height: '65%',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 10,
+            height: 10,
+        },
+        shadowOpacity: 0.2,
+        shadowRadius: 10,
+    },
+    bg: {
+        position: 'absolute',
+        backgroundColor: '#fcd104',
+        width: '100%',
+        height: '30%',
     },
 });
 
