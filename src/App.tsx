f308f926ad604bac68a946e7eb3114e9f2f90d0f
Darius <dariusz.sobkowicz@gmail.com>
2020-07-02 15:25:46 +0200

    ios and android splash screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Root} from 'native-base';
 import {Provider} from 'react-redux';
+import SplashScreen from 'react-native-splash-screen';
 
 import MainNavigator from './navigation/MainNavigator';
 import store from './store';
 
 export default function App() {
+    useEffect(() => {
+        SplashScreen.hide();
+    }, []);
     return (
         <Provider store={store}>
             <Root>
