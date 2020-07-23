37bff48665c099a11dfa94a4549b5b5dc5dc0210
Darius <dariusz.sobkowicz@gmail.com>
2020-07-23 18:00:41 +0200

    enter address buttons and errors component

--- src/components/EnterAddress/EnterAddress.tsx
+++ src/components/EnterAddress/EnterAddress.tsx
@@ -1,10 +1,11 @@
 import React, {useState, FunctionComponent} from 'react';
 import {StyleSheet, Clipboard} from 'react-native';
-import {Text, View, Input, Item, Icon} from 'native-base';
+import {Text, View, Input, Item} from 'native-base';
 
-import CustomButton from '../CustomButton';
 import CustomHeader from '../CustomHeader';
 import EnterAddressCamera from './EnterAddressCamera';
+import EnterAddressErrors from './EnterAddressErrors';
+import EnterAddressButtons from './EnterAddressButtons';
 
 interface EnterAddressProps {
     headerTitle: string;
@@ -86,44 +87,15 @@ const EnterAddress: FunctionComponent<EnterAddressProps> = ({
                         </Item>
                     </View>
                     {children}
-                    {isError && (
-                        <View style={styles.error}>
-                            <View style={styles.errorTitle}>
-                                <Icon
-                                    name="warning"
-                                    type="AntDesign"
-                                    style={styles.errorIcon}
-                                />
-                                <Text style={styles.typo3}>
-                                    Invalid Tezos Address
-                                </Text>
-                            </View>
-                            <View style={styles.errorText}>
-                                <Text style={styles.typo4}>{errorMessage}</Text>
-                            </View>
-                        </View>
-                    )}
-                    {!isValid && (
-                        <View style={styles.actions}>
-                            <View>
-                                <CustomButton
-                                    icon="Paste"
-                                    label="Paste Address"
-                                    color="#f5942a"
-                                    onPress={onPasteAddress}
-                                />
-                            </View>
-                            <View style={styles.actionLine} />
-                            <View>
-                                <CustomButton
-                                    icon="Scan"
-                                    label="Scan QR Code"
-                                    color="#f5942a"
-                                    onPress={onScanQrCode}
-                                />
-                            </View>
-                        </View>
-                    )}
+                    <EnterAddressErrors
+                        isVisible={isError}
+                        message={errorMessage}
+                    />
+                    <EnterAddressButtons
+                        isVisible={!isValid}
+                        onPasteAddress={onPasteAddress}
+                        onScanQrCode={onScanQrCode}
+                    />
                 </>
             )}
         </>
@@ -150,98 +122,6 @@ const styles = StyleSheet.create({
         fontFamily: 'Roboto-Light',
         fontSize: 18,
         fontWeight: '300',
-    },
-    error: {
-        marginTop: 30,
-        width: 259,
-        backgroundColor: '#f30000',
-        alignSelf: 'center',
-        borderRadius: 15.5,
-        padding: 12,
-    },
-    errorTitle: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    errorText: {
-        marginTop: 3,
-        alignSelf: 'center',
-    },
-    errorIcon: {
-        color: '#ffffff',
-        fontSize: 14,
-        marginRight: 8,
-    },
-    actions: {
-        marginTop: 'auto',
-        paddingVertical: 42,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        backgroundColor: '#ffffff',
-        borderTopLeftRadius: 26,
-        borderTopRightRadius: 26,
-    },
-    actionLine: {
-        width: 1,
-        backgroundColor: '#e8e8e8',
-        marginHorizontal: 50,
-    },
-    next: {
-        flexDirection: 'row',
-        flexGrow: 1,
-        paddingHorizontal: 35,
-        alignItems: 'center',
-    },
-    nextCircle: {
-        width: 36,
-        height: 36,
-        borderStyle: 'solid',
-        borderWidth: 2,
-        borderColor: '#fcd104',
-        borderRadius: 18,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginRight: 12,
-    },
-    nextButton: {
-        marginLeft: 'auto',
-        width: 128,
-        height: 50,
-        backgroundColor: '#4b4b4b',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 25,
-    },
-    camera: {
-        flex: 1,
-        width: '100%',
-    },
-    typo1: {
-        fontFamily: 'Roboto-Light',
-        fontSize: 14,
-        color: '#646464',
-        marginBottom: 5,
-    },
-    typo2: {
-        fontFamily: 'Roboto-Medium',
-        fontSize: 17,
-        fontWeight: '500',
-        letterSpacing: 0.85,
-    },
-    typo3: {
-        fontFamily: 'Roboto-Bold',
-        fontSize: 14,
-        fontWeight: 'bold',
-        letterSpacing: 0,
-        color: '#ffffff',
-    },
-    typo4: {
-        fontFamily: 'Roboto-Regular',
-        fontSize: 14,
-        fontWeight: 'normal',
-        letterSpacing: 0,
-        color: '#ffffff',
     },
 });
 
