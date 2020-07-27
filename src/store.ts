13d3f4cbbe4c417c8f4a63c8e5b624ce5c488bdb
anonymoussprocket <anonymoussprocket@gmail.com>
2020-07-27 19:25:16 -0400

    - version - remove flipper for android

--- src/store.ts
+++ src/store.ts
@@ -5,10 +5,4 @@ import rootReducer from './reducers';
 
 const middlewares = [ReduxThunk];
 
-// Redux Debugger Plugin for Flipper
-if (__DEV__) {
-    const createDebugger = require('redux-flipper').default;
-    middlewares.push(createDebugger());
-}
-
 export default createStore(rootReducer, applyMiddleware(...middlewares));
