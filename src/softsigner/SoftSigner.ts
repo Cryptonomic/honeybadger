bd9cc9f7bc17fca9d3a19f20641ef8c798a1f49d
anonymoussprocket <anonymoussprocket@gmail.com>
2020-10-14 01:51:26 -0400

    - update to match conseiljs 505

--- src/softsigner/SoftSigner.ts
+++ src/softsigner/SoftSigner.ts
@@ -1,4 +1,4 @@
-import {Signer, TezosMessageUtils} from 'conseiljs';
+import {Signer, SignerCurve, TezosMessageUtils} from 'conseiljs';
 
 import * as CryptoUtils from './utils/CryptoUtils';
 import {Buffer} from 'buffer';
@@ -9,6 +9,11 @@ export default class SoftSigner implements Signer {
     constructor(secretKey: Buffer) {
         this.secretKey = secretKey;
     }
+
+    public getSignerCurve(): SignerCurve {
+        return SignerCurve.ED25519
+    }
+
     public async signOperation(bytes: Buffer): Promise<Buffer> {
         return await CryptoUtils.signDetached(
             TezosMessageUtils.simpleHash(bytes, 32),
@@ -17,10 +22,20 @@ export default class SoftSigner implements Signer {
     }
 
     public async signText(message: string): Promise<string> {
-        const messageSig = await CryptoUtils.signDetached(
-            Buffer.from(message, 'utf8'),
-            this.secretKey,
-        );
+        const messageSig = await CryptoUtils.signDetached(Buffer.from(message, 'utf8'), this.secretKey);
+
+        return TezosMessageUtils.readSignatureWithHint(messageSig, 'edsig');
+    }
+
+    /**
+     * * Convenience function that uses Tezos nomenclature to sign arbitrary text. This method produces a 32-byte blake2s hash prior to signing.
+     * 
+     * @param message UTF-8 text
+     * @returns {Promise<string>} base58check-encoded signature prefixed with 'edsig'
+     */
+    public async signTextHash(message: string): Promise<string> {
+        const messageHash = TezosMessageUtils.simpleHash(Buffer.from(message, 'utf8'), 32);
+        const messageSig = await CryptoUtils.signDetached(messageHash, this.secretKey);
 
         return TezosMessageUtils.readSignatureWithHint(messageSig, 'edsig');
     }
