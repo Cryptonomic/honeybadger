b242027cd9daeb5ec392cfaca98c522cc4bc647f
anonymoussprocket <anonymoussprocket@gmail.com>
2021-01-05 20:48:19 -0500

    - removed config - updated gitignore

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 export default [
     {
-        explorerUrl: 'https://tezblock.io/transaction',
-        nodeUrl: 'https://tezos-dev.cryptonomic-infra.tech:443',
-        url: 'https://conseil-dev.cryptonomic-infra.tech:443',
-        apiKey: 'galleon',
-        network: 'delphinet',
-        displayNetwork: 'delphinet',
+        explorerUrl: '',
+        nodeUrl: '',
+        url: '',
+        apiKey: '',
+        network: 'mainnet',
+        displayNetwork: 'mainnet',
     },
 ];
